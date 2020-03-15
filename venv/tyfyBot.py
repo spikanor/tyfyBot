07c1ff7c5708041c897d267a238376b03e29bcbc
jrx <jrx@uw.edu>
2020-03-14 19:26:22 -0500

    feat(check_twitch_live): Added feature to send message when streamer goes live
    -Sends message into specified live stream channel mentioning streamer
    and displaying stream link.

--- venv/tyfyBot.py
+++ venv/tyfyBot.py
@@ -3,37 +3,69 @@ from discord.ext import commands
 import pymongo
 from pymongo import MongoClient
 import requests
+import asyncio
 
 
 # Constants
-token = 'Njg4MTQwMzU1NjQ3ODMyMDky.Xmv-tA.HdFiF2oc9HoARLe2dVIwBrR6dDw'
-twitch_streams_url = "https://api.twitch.tv/helix/streams"
-twitch_users_url = "https://api.twitch.tv/helix/users"
+TOKEN = 'REMOVED'
+TWITCH_URL = "https://twitch.tv/"
+TWITCH_STREAMS_URL = "https://api.twitch.tv/helix/streams"
+TWITCH_USERS_URL = "https://api.twitch.tv/helix/users"
 
-discord_client = commands.Bot(command_prefix='ty!')
+LIVE_STREAMS_CHANNEL = "live-streams"
+TWITCH_STREAMER_ROLE = "Twitch Streamer"
+ADMIN_ROLE = "admin"
+
 mongo_client = MongoClient("mongodb+srv://justinxu:gunsnrosesomg123@cluster0-phv8p.mongodb.net/test?retryWrites=true&w=majority")
+twitch_db = mongo_client["tyfyBot"]["Twitch"]
+discord_client = commands.Bot(command_prefix='ty!')
 
 
 # Events
 @discord_client.event
 async def on_ready():
-    print("Ready")
+    print("\tReady")
+
+@discord_client.event
+async def check_twitch_live():
+    await discord_client.wait_until_ready()
+    while not discord_client.is_closed():
+        streamers = twitch_db.find({})
+        for streamer in streamers:
+            data = twitch_GET(TWITCH_STREAMS_URL, {"user_login" : streamer["twitch_name"]})
+            query = {"twitch_name" : streamer["twitch_name"], "guild_id" : streamer["guild_id"]}
+            print("Searching for " + streamer["twitch_name"] + " in guild " + discord_client.get_guild(streamer["guild_id"]).name + "...")
+            if (data["data"] and not streamer["is_live"]):
+                twitch_db.update_one(query, {"$set": {"is_live": True}})
+                streamer_guild = discord_client.get_guild(streamer["guild_id"])
+                streamer_member = streamer_guild.get_member_named(streamer["discord_name"])
+                streams_channel = discord.utils.get(streamer_guild.text_channels, name=LIVE_STREAMS_CHANNEL)
+                if (streams_channel):
+                    await streams_channel.send(streamer_member.mention + " is now live at " + TWITCH_URL + streamer["twitch_name"])
+            elif (not data["data"] and streamer["is_live"]):
+                twitch_db.update_one(query, {"$set": {"is_live": False}})
+        print("\t\tsleeping...")
+        await asyncio.sleep(5)
+
 
 # Commands
 @discord_client.command(pass_context=True)
 async def twitch_name(ctx, twitch_name=""):
-    if (isPrivate(ctx)):
+    if (is_private(ctx)):
         await ctx.send("Use this command in a server.")
-    elif (discord.utils.get(ctx.guild.roles, name="Twitch") not in ctx.message.author.roles):
-        await ctx.send("Must have the 'Twitch' role to use this command.")
+    elif (not has_role(ctx, TWITCH_STREAMER_ROLE)):
+        await ctx.send("Must have the '" + TWITCH_STREAMER_ROLE + "' role to use this command.")
     else:
-        twitch_db = mongo_client[ctx.message.channel.guild.name]["Twitch"]
-        discord_name = ctx.message.author.name
-        query = {"discord_name": discord_name}
-            
+        if (not is_clean_input(twitch_name)):
+            await ctx.send("Please enter ASCII characters only.")
+            return
+        guild_id = ctx.message.channel.guild.id
+        discord_name = str(ctx.message.author)
+        query = {"discord_name": discord_name, "guild_id" : guild_id}
+
         # Set twitch name
         if (twitch_name):
-            data = twitch_GET(twitch_users_url, {"login": twitch_name})
+            data = twitch_GET(TWITCH_USERS_URL, {"login": twitch_name})
             if (not data["data"]):
                 await ctx.send("Invalid username.")
             else:
@@ -41,7 +73,8 @@ async def twitch_name(ctx, twitch_name=""):
                     twitch_db.update_one(query, {"$set": {"twitch_name": twitch_name}})
                     await ctx.send("Updated!")
                 else:
-                    twitch_db.insert_one({"discord_name" : discord_name, "twitch_name" : twitch_name})
+                    twitch_db.insert_one({"discord_name" : discord_name, "twitch_name" : twitch_name,
+                                          "guild_id" : guild_id, "is_live" : False})
                     await ctx.send("Twitch name is now " + twitch_name + ".")
         # Query twitch name
         else:
@@ -51,15 +84,31 @@ async def twitch_name(ctx, twitch_name=""):
             else:
                 await ctx.send("Twitch name not set.")
 
+@discord_client.command(pass_context=True)
+async def close(ctx):
+    if (is_private(ctx)):
+        await ctx.send("Must use command in server.")
+    elif (not has_role(ctx, ADMIN_ROLE)):
+        await ctx.send("Must have '" + ADMIN_ROLE + "' to use this command.")
+    else:
+        await ctx.send("**CY@**")
+        await discord_client.close()
 
 # Helpers
-def isPrivate(ctx):
+def is_private(ctx):
     return ctx.message.channel.type == discord.ChannelType.private
+
+def has_role(ctx, role_name):
+    return discord.utils.get(ctx.guild.roles, name=role_name) in ctx.message.author.roles
 
 def twitch_GET(url, params):
     header = {"Client-ID": 't0fw98983za1rmuv7pfqli0wrta1hd'}
     request = requests.get(url=url, params=params, headers=header)
     return request.json()
 
+def is_clean_input(inputString):
+    return len(inputString) == len(inputString.encode())
 
-discord_client.run(token)
+
+discord_client.loop.create_task(check_twitch_live())
+discord_client.run(TOKEN)
